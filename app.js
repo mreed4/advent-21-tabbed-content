6d3f771e756cbd903dc7263ee65e36b295940438
Matthew <78713317+mreed4@users.noreply.github.com>
2024-01-15 20:42:10 -0800

    Project complete -- all functionality works

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ function renderEpisodes() {
     /* */
     const episodeLi = buildEpisode(episode, i);
     episodesUl.appendChild(episodeLi);
+
+    episodeLi.addEventListener("click", () => {
+      markEpisodeAsSelected(episodeLi);
+      renderEpisodeDetails(episode);
+    });
   });
 }
 
@@ -30,4 +35,21 @@ function buildEpisode(episode, i) {
     </a>`;
 
   return episodeLi;
+}
+
+function markEpisodeAsSelected(episodeLi) {
+  const selectedEpisode = document.querySelector(".selected");
+  selectedEpisode.classList.remove("selected");
+
+  episodeLi.classList.add("selected");
+}
+
+function renderEpisodeDetails(episode) {
+  const episodeDetails = document.querySelector("main");
+  const cover = episodeDetails.querySelector(".cover img");
+  const h1 = episodeDetails.querySelector("h1");
+  const p = episodeDetails.querySelector("p");
+  cover.src = `./images/cover__episode-${episode.id}.png`;
+  h1.innerText = episode.title;
+  p.innerText = episode.description;
 }
