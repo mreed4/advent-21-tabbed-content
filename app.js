749bbf76f586b291e6de1d3b2dedb55307212990
Matthew <78713317+mreed4@users.noreply.github.com>
2024-01-15 20:27:28 -0800

    Pull episodes from data structure

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 import { episodes } from "./data.js";
 
-const tabsUl = document.querySelector("#tabs");
+(function init() {
+  console.log("init");
+  renderEpisodes();
+})();
 
-(function init() {})();
+function renderEpisodes() {
+  /* */
+  const episodesUl = document.querySelector("#episodes");
+
+  episodes.forEach((episode, i) => {
+    /* */
+    const episodeLi = buildEpisode(episode, i);
+    episodesUl.appendChild(episodeLi);
+  });
+}
+
+function buildEpisode(episode, i) {
+  const episodeLi = document.createElement("li");
+
+  if (i === 0) {
+    episodeLi.classList.add("selected");
+  }
+
+  episodeLi.innerHTML = `
+    <a href="#">
+      <div class="episode">Episode ${episode.id}</div>
+      <div class="title">${episode.title}</div>
+    </a>`;
+
+  return episodeLi;
+}
